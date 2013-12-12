9f68789b985a12d71b12ac2f866372c1bae0b169
Bastien Brunnenstein <bastien@brunnenstein.fr>
2013-12-12 01:49:23 +0100

    Updated GameScreen to draw and handle A and B buttons using Input class

--- src/com/brakassey/sunproject/screens/GameScreen.java
+++ src/com/brakassey/sunproject/screens/GameScreen.java
@@ -1,16 +1,40 @@
 package com.brakassey.sunproject.screens;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL11;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.brakassey.sunproject.utils.Input;
 
 public class GameScreen implements Screen {
+
+    private Game m_game;
+
+    private Input m_input = new Input();
+    SpriteBatch m_batch = new SpriteBatch();
+
+    public GameScreen(Game game) {
+        m_game = game;
+        m_batch.enableBlending();
+        m_batch.setBlendFunction(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+    }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 1) ;
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT) ;
 
+        // TODO Update game
+        m_input.update();
+
+        // TODO Render game
+        m_batch.begin();
+
+        m_input.renderLayout(m_batch);
+
+        m_batch.end();
     }
 
     @Override
--- src/com/brakassey/sunproject/screens/MenuScreen.java
+++ src/com/brakassey/sunproject/screens/MenuScreen.java
@@ -4,14 +4,20 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL11;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MenuScreen implements Screen {
 
     private Game m_game;
 
+    private SpriteBatch m_batch = new SpriteBatch();
+
     public MenuScreen(Game game) {
         m_game = game;
+        m_batch.enableBlending();
+        m_batch.setBlendFunction(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
     }
 
     @Override
@@ -19,9 +25,11 @@ public class MenuScreen implements Screen {
         Gdx.gl.glClearColor(1, 1, 1, 1) ;
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT) ;
 
-
         if (Gdx.input.isKeyPressed(Keys.ENTER))
-            m_game.setScreen(new GameScreen());
+            m_game.setScreen(new GameScreen(m_game));
+
+		m_batch.begin();
+		m_batch.end();
     }
 
     @Override
