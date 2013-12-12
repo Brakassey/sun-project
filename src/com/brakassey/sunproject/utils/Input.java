3ef56cfe49179a757637cde801b19e751e4608aa
Bastien Brunnenstein <bastien@brunnenstein.fr>
2013-12-12 13:48:57 +0100

    DPad now functional in Input class

--- src/com/brakassey/sunproject/utils/Input.java
+++ src/com/brakassey/sunproject/utils/Input.java
@@ -8,12 +8,13 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Input {
-    
+
     /**
      * Configuration for buttons.
      */
     final int MARGIN = 24;
     final int SIZE = 96;
+    final int DSIZE = SIZE * 2;
 
     /**
      * Directions.
@@ -40,9 +41,11 @@ public class Input {
 
     private Direction m_direction       = Direction.NONE;
     private boolean m_buttons[]         = new boolean[4];
-    
+
+    private Texture DPad_button;
     private Texture A_button;
     private Texture B_button;
+    private Sprite DPad;
     private Sprite A;
     private Sprite B;
 
@@ -52,13 +55,16 @@ public class Input {
         m_buttons[1] = false;
         m_buttons[2] = false;
         m_buttons[3] = false;
-        
+
+        DPad_button = new Texture("img/ui/dpad.png");
         A_button = new Texture("img/ui/A.png");
         B_button = new Texture("img/ui/B.png");
+        DPad = new Sprite(DPad_button);
         A = new Sprite(A_button);
         B = new Sprite(B_button);
 
         int screenw = Gdx.graphics.getWidth();
+        DPad.setBounds(MARGIN, MARGIN, DSIZE, DSIZE);
         A.setBounds(screenw - MARGIN - SIZE, MARGIN * 2, SIZE, SIZE);
         B.setBounds(screenw - MARGIN * 2 - SIZE * 2, MARGIN, SIZE, SIZE);
     }
@@ -120,26 +126,58 @@ public class Input {
     public void update() {
         int screenw = Gdx.graphics.getWidth();
         int screenh = Gdx.graphics.getHeight();
-        
+
         if (Gdx.input.isPeripheralAvailable(Peripheral.MultitouchScreen))
         {
             // TODO TOUCHSCREEN CONTROLS
             setState(Button.A, false);
             setState(Button.B, false);
+            setDirection(Direction.NONE);
             for (int i = 0; i < 5; ++i)
             {
                 if (Gdx.input.isTouched(i))
                 {
                     int x = Gdx.input.getX(i);
                     int y = screenh - Gdx.input.getY(i);
-                    
-                    if (x > (screenw - MARGIN - SIZE)
-                     && x < (screenw - MARGIN)
-                     && y > (MARGIN * 2)
-                     && y < (MARGIN * 2 + SIZE))
+
+                    // DPAD
+                    if (x > (MARGIN)
+                     && x < (MARGIN + DSIZE)
+                     && y > (MARGIN)
+                     && y < (MARGIN + DSIZE))
+                    {
+                        x -= MARGIN + (DSIZE / 2);
+                        y -= MARGIN + (DSIZE / 2);
+
+                        // Dead zone in the center
+                        if (Math.abs(x) + Math.abs(y) < DSIZE / 10)
+                            continue;
+
+                        boolean hori = Math.abs(x) > Math.abs(y);
+
+                        if (hori && (x > 0))
+                            setDirection(Direction.RIGHT);
+
+                        else if (hori && (x <= 0))
+                            setDirection(Direction.LEFT);
+
+                        else if (y > 0)
+                            setDirection(Direction.UP);
+
+                        else // y <= 0
+                            setDirection(Direction.DOWN);
+                    }
+
+                    // A
+                    else if (x > (screenw - MARGIN - SIZE)
+                          && x < (screenw - MARGIN)
+                          && y > (MARGIN * 2)
+                          && y < (MARGIN * 2 + SIZE))
                     {
                         setState(Button.A, true);
                     }
+
+                    // B
                     else if ((x > screenw - MARGIN * 2 - SIZE * 2)
                           && (x < screenw - MARGIN * 2 - SIZE)
                           && (y > MARGIN)
@@ -174,19 +212,25 @@ public class Input {
                 setDirection(Direction.NONE);
         }
     }
-    
+
     public void renderLayout(SpriteBatch batch)
     {
+        if (getDirection() != Direction.NONE)
+            DPad.setColor(1, 1, 1, 0.3f);
+        else
+            DPad.setColor(1, 1, 1, 0.6f);
+
         if (isDown(Button.A))
             A.setColor(1, 1, 1, 0.3f);
         else
             A.setColor(1, 1, 1, 0.6f);
-        
+
         if (isDown(Button.B))
             B.setColor(1, 1, 1, 0.3f);
         else
             B.setColor(1, 1, 1, 0.6f);
-    
+
+        DPad.draw(batch);
         A.draw(batch);
         B.draw(batch);
     }
