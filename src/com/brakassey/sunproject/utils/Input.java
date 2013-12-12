69cb302bef8691fb73f0cf163ba3fad02ee0a991
Bastien Brunnenstein <bastien@brunnenstein.fr>
2013-12-12 01:48:23 +0100

    Input class now handling A and B buttons

--- src/com/brakassey/sunproject/utils/Input.java
+++ src/com/brakassey/sunproject/utils/Input.java
@@ -3,8 +3,17 @@ package com.brakassey.sunproject.utils;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Input.Peripheral;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Input {
+    
+    /**
+     * Configuration for buttons.
+     */
+    final int MARGIN = 24;
+    final int SIZE = 96;
 
     /**
      * Directions.
@@ -31,6 +40,11 @@ public class Input {
 
     private Direction m_direction       = Direction.NONE;
     private boolean m_buttons[]         = new boolean[4];
+    
+    private Texture A_button;
+    private Texture B_button;
+    private Sprite A;
+    private Sprite B;
 
     public Input()
     {
@@ -38,6 +52,15 @@ public class Input {
         m_buttons[1] = false;
         m_buttons[2] = false;
         m_buttons[3] = false;
+        
+        A_button = new Texture("img/ui/A.png");
+        B_button = new Texture("img/ui/B.png");
+        A = new Sprite(A_button);
+        B = new Sprite(B_button);
+
+        int screenw = Gdx.graphics.getWidth();
+        A.setBounds(screenw - MARGIN - SIZE, MARGIN * 2, SIZE, SIZE);
+        B.setBounds(screenw - MARGIN * 2 - SIZE * 2, MARGIN, SIZE, SIZE);
     }
 
     public Direction getDirection()
@@ -95,10 +118,37 @@ public class Input {
     }
 
     public void update() {
+        int screenw = Gdx.graphics.getWidth();
+        int screenh = Gdx.graphics.getHeight();
+        
         if (Gdx.input.isPeripheralAvailable(Peripheral.MultitouchScreen))
         {
             // TODO TOUCHSCREEN CONTROLS
-
+            setState(Button.A, false);
+            setState(Button.B, false);
+            for (int i = 0; i < 5; ++i)
+            {
+                if (Gdx.input.isTouched(i))
+                {
+                    int x = Gdx.input.getX(i);
+                    int y = screenh - Gdx.input.getY(i);
+                    
+                    if (x > (screenw - MARGIN - SIZE)
+                     && x < (screenw - MARGIN)
+                     && y > (MARGIN * 2)
+                     && y < (MARGIN * 2 + SIZE))
+                    {
+                        setState(Button.A, true);
+                    }
+                    else if ((x > screenw - MARGIN * 2 - SIZE * 2)
+                          && (x < screenw - MARGIN * 2 - SIZE)
+                          && (y > MARGIN)
+                          && (y < MARGIN + SIZE))
+                    {
+                        setState(Button.B, true);
+                    }
+                }
+            }
         }
         else
         {
@@ -123,6 +173,22 @@ public class Input {
             else
                 setDirection(Direction.NONE);
         }
+    }
+    
+    public void renderLayout(SpriteBatch batch)
+    {
+        if (isDown(Button.A))
+            A.setColor(1, 1, 1, 0.3f);
+        else
+            A.setColor(1, 1, 1, 0.6f);
+        
+        if (isDown(Button.B))
+            B.setColor(1, 1, 1, 0.3f);
+        else
+            B.setColor(1, 1, 1, 0.6f);
+    
+        A.draw(batch);
+        B.draw(batch);
     }
 
 }
